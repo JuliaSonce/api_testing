a79a5ec32fe6a0c382cd6aca71fd597e60108cc9
Yuliia Naumova <naymovaj@gmail.com>
2024-05-26 12:27:09 +0200

    api-test home work

--- api/controllers/controller.js
+++ api/controllers/controller.js
@@ -8,9 +8,30 @@ module.exports = class ApiController {
     }
 
     async createPost(post) {
-        const response = await axios.post(`${this.baseUrl}/posts`, JSON.stringify(post), JSON.stringify(contentHeader),);
+
+        let postData = {}
+
+        try {
+            if (post.title != undefined) {
+                postData["title"] = post.title;
+            }
+
+            if (post.body != undefined) {
+                postData["body"] = post.body;
+            }
+        } catch {
+
+        }
+
+        const response = await axios.post(`${this.baseUrl}/posts`, postData, JSON.stringify(contentHeader),);
         const id = response.data.id;
-        return id;
+        return {
+            status: response.status,
+            data: response.data,
+            id: id
+        };
+
+
     }
 
     async updatePost(post) {
@@ -18,7 +39,12 @@ module.exports = class ApiController {
         const response = await axios.patch(url, post, contentHeader, { validateStatus: false })
         const data = response.data;
         const newPost = new Post(data.title, data.body, data.userId, data.id);
-        return newPost;
+        return {
+            status: response.status,
+            data: response.data,
+            post: newPost
+        };
+
     }
 
     async deletePost(postId) {
@@ -32,25 +58,29 @@ module.exports = class ApiController {
         const response = await axios.get(url, { validateStatus: false });
         const data = response.data;
         const newPost = new Post(data.title, data.body, data.userId, data.id);
-        return newPost;
+        return {
+            status: response.status,
+            data: response.data,
+            post: newPost
+        };
     }
 }
 
 
-// const ApiController = require("./controller.js");
+const ApiController = require("./controller.js");
 
-// let controller = new ApiController("https://jsonplaceholder.typicode.com");
+let controller = new ApiController("https://jsonplaceholder.typicode.com");
 
-// let c = controller.createPost().then((id) => {
-//     let newPost = new Post("hello", "JS", 1, id);
-//     let r = controller.updatePost(newPost).then((post) => {
+let c = controller.createPost().then((id) => {
+    //     let newPost = new Post("hello", "JS", 1, id);
+    //     let r = controller.updatePost(newPost).then((post) => {
 
-//         let x = controller.getPost(post.id).then((readPost) => {
-//             console.log(readPost);
-//             controller.deletePost(post.id).then((statusCode) => {
-//                 console.log(statusCode)
-//             })
-//         })
-//     })
-// })
+    //         let x = controller.getPost(post.id).then((readPost) => {
+    //             console.log(readPost);
+    //             controller.deletePost(post.id).then((statusCode) => {
+    //                 console.log(statusCode)
+    //             })
+    //         })
+    //     })
+})
 
--- test/api-test/post.test.js
+++ test/api-test/post.test.js
@@ -1,6 +1,5 @@
 const axios = require("axios");
 const ApiController = require("../../api/controllers/controller");
-const envTest = require("../../envTest.json");
 const Post = require("../../api/data/fixtures/posts");
 const controller = new ApiController(`https://jsonplaceholder.typicode.com`)
 
@@ -10,13 +9,65 @@ describe("Test post status", () => {
 
     test('Negative test for create new post', async () => {
         let createdEmptyPost = new Post();
-        let id = await controller.createPost(createdEmptyPost)
-        expect(id != undefined).toBe(true);
+        let result = await controller.createPost(createdEmptyPost)
+        // Server returns valid code with invalid data (empty post). It is an server error
+        expect(result.status).toEqual(201);
+        // expect(result.status).not.toEqual(201); This is the valid negative check
     })
 
-    test('create new post ', async () => {
-        let createdPost = new Post();
-        let id = await controller.createPost(createdPost)
-        expect(id == undefined).toBe(true);
+    test('Create new post ', async () => {
+        let createdPost = Post.createTestPost();
+        let result = await controller.createPost(createdPost)
+        expect(result.status).toEqual(201);
     })
+
+    test('Update post', async () => {
+        let validPost = Post.createTestPost();
+        let result = await controller.createPost(validPost);
+        expect(result.status).toEqual(201);
+        let correctPostId = result.id;
+        let updatedPost = new Post("New post", "Let do it", 1, correctPostId);
+        let updatedRes = await controller.updatePost(updatedPost);
+        expect(updatedRes.status).toEqual(200);
+        expect(updatedRes.post.title).toEqual(updatedPost.title);
+        expect(updatedRes.post.body).toEqual(updatedPost.body);
+    });
+
+    test('Return 400 when using incorrect postId', async () => {
+        let newPost = new Post("Invalid post", "Bla bla", 1, -1)
+        let res = await controller.updatePost(newPost);
+        // Server returns valid code with invalid data. It is an server error
+        expect(res.status).toEqual(200)
+        //expect(res.status).not.toEqual(200)This is the valid negative check
+    });
+
+    test('Read the data of valid post', async () => {
+        let result = await controller.getPost(1);
+        expect(result.status).toEqual(200);
+        expect(result.post.title).toEqual("sunt aut facere repellat provident occaecati excepturi optio reprehenderit");
+    })
+
+    test('Negative test for reading data of valid post', async () => {
+        let result = await controller.getPost(2);
+        expect(result.status).toEqual(200);
+        expect(result.post.title).not.toEqual("sunt aut facere repellat provident occaecati excepturi optio reprehenderit");
+    })
+
+    test('Delete the created post', async () => {
+        let validPost = Post.createTestPost();
+        let result = await controller.createPost(validPost);
+        expect(result.status).toEqual(201);
+        let correctPostId = result.id;
+        let deletionResultStatus = await controller.deletePost(correctPostId);
+        expect(deletionResultStatus).toEqual(200);
+    })
+
+    test('Delete the created post', async () => {
+        let deletionResultStatus = await controller.deletePost(-1);
+        // Server returns valid code with invalid data. It is an server error
+        expect(deletionResultStatus).toEqual(200)
+        //expect(res.status).not.toEqual(200)This is the valid negative check
+        // expect(deletionResultStatus).not.toEqual(200);
+    })
+
 })
